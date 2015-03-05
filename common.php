d1a5afceb91514cdf45aadb633776ecbbac00709
devsmt <devsmt@gmail.com>
2015-03-05 14:59:28 +0100

    initial commit

---
+++ common.php
@@ -0,0 +1,8 @@
+<?php
+
+function ip2country(){
+    // - scorrere tutti gli ip di una rete e popolare un db sqlite con ip_inizio, ip_fine, country code
+    // - saltare gli ip che appartengono a una sottorete
+    // dato un db country=>ip list, genereare file host.deny data una lista di country
+    $output = shell_exec('whois '.$ip.' -H | grep country | awk \'{print $2}\'');
+}
