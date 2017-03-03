2091340203fb24ace51bd86b40e87519551f2f6d
devsmt <devsmt@gmail.com>
2017-03-03 10:07:25 +0100

    better IP parsing

--- common.php
+++ common.php
@@ -1,8 +1,9 @@
 <?php
 
-function ip2country(){
+function ip2country($ip){
+    $ip = safe_IP($ip);
     // - scorrere tutti gli ip di una rete e popolare un db sqlite con ip_inizio, ip_fine, country code
     // - saltare gli ip che appartengono a una sottorete
     // dato un db country=>ip list, genereare file host.deny data una lista di country
-    $output = shell_exec('whois '.$ip.' -H | grep country | awk \'{print $2}\'');
+    $output = `whois $ip  -H | grep country | awk \'{print $2}\''`;
 }
